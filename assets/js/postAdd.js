d01d6c5a6859d242df6df194f7aa6ac92d6f6c77
wuhu <wuhu@itcast.cn>
2019-09-22 11:06:04 +0800

    15-富文本框的使用

--- assets/js/postAdd.js
+++ assets/js/postAdd.js
@@ -1,34 +1,50 @@
 $(function(){
     // 实现文件上传
     $('#feature').on('change',function(){
-        // ajax所支持的数据的常见格式：
-        // key=value&key=value
-        // {}
-        // FormData对象
-        // 获取文件对象  files:当前用户所选择的文件列表，里面的值就是用户选择的文件对象--blob
+        // 获取用户所选择的文件
+        // files:用户所选择文件列表,它是一个伪数组
         let myfile = $('#feature')[0].files[0]
-        // 使用formdata收集数据
-        let formdata = new FormData()
-        // 添加数据到formdata
-        // blob:二进制文件对象：binary large object
+        var formdata = new FormData()
+        // blob:二进制的大文件--文件
         formdata.append('img',myfile)
-        formdata.append('username',"jack_37")
+        formdata.append('myvalue','什么值都可以，只要是字符串')
 
-        // 使用ajax实现文件上传请求
-        // 凡是 formdata结合ajax实现请求，一定需要设置两个属性：
+        // 发起ajax请求
+        // 支持的data的常见格式
+        // key=value&key=value:$('form').serialize()
+        // 对象
+        // formdata
         $.ajax({
             type:'post',
             url:'/uploadFile',
             data:formdata,
-            processData:false, // 告诉ajax不要进行数据的处理，因为formdata已经进行处理了
-            contentType:false, // 告诉ajax不要对数据进行编码处理，因为formdata已经进行处理了
             dataType:'json',
+            processData:false,
+            contentType:false,
             success:function(res){
                 console.log(res)
                 if(res.code == 200){
+                    // 1.预览
                     $('.thumbnail').attr('src','/'+res.img).show()
+                    // 2.将当前图片路径存储到隐藏域，方便后期的参数获取
+                    $('[name="feature"]').val('/'+res.img)
                 }
             }
         })
+    })
+
+    // 初始化富文本框
+    // 下面这句代码的作用是：创建一个富文本框控件(实例对象)来覆盖你所指定的id号为content的文本域
+    CKEDITOR.replace('content')
+
+    // 实现文件新增
+    $('.btnAdd').on('click',function(){
+        // CKEDITOR.instances:可以获取当前页面中所有的富文本框对象集合
+        // CKEDITOR.instances.content：获取id号为content的富文本框对象
+        // console.log(CKEDITOR.instances.content.getData())
+        // 实现富文本框和文本域的数据同步
+        CKEDITOR.instances.content.updateElement()
+        console.log($('form').serialize())
+        // 1.使用getData
     })
 })
--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -1,24 +1,15 @@
 // 这个文件专门用于处理文件上传业务
 const formidable = require('formidable')
 module.exports = {
-    uploadFile(req,res){
+    uploadFile(req, res) {
         // 使用formidable实现文件上传
-        // 1.创建文件上传对象
         let form = new formidable.IncomingForm()
-        // 2.配置
-        // 2.1 编码：这个模块可以实现文件上传，也可以传递普通的键值对
+        // 配置
         form.encoding = 'utf-8'
-        // 2.2 设置文件上传之后的存储目录
-        form.uploadDir = './uploads'
-        // 2.3 设置是否保留扩展名
         form.keepExtensions = true
-        // 3 实现文件上传
-        // req:请求报文，文件上传的数据就在请求报文对象中
-        // 回调函数：文件上传完成时触发的回调函数
-        // err:文件上传失败
-        // fields：传递的普通键值对
-        // files：文件上传成功之后的相关信息
-        form.parse(req,(err,fields,files)=>{
+        form.uploadDir = './uploads'
+        // 实现文件上传操作
+        form.parse(req,(err,fields,files) => {
             if(err){
                 res.json({
                     code:400,
@@ -26,10 +17,11 @@ module.exports = {
                 })
             }else{
                 console.log(fields)
-                console.log(files.img.path)
+                console.log(files)
                 res.json({
                     code:200,
                     msg:'文件上传成功',
+                    // 将文件在服务器中的存储路径返回
                     img:files.img.path
                 })
             }
